9afdf69e5513d98badfaefd4c6061f3a6ec43b1a
kundan kumar <kundankumarkondil1999@gmail.com>
2024-02-11 17:07:59 +0530

    added array

--- HTMLCSSJS/admissionapp/index.js
+++ HTMLCSSJS/admissionapp/index.js
@@ -3,19 +3,19 @@ function admission(
   stu_advance,
   course_name,
   query,
-  age,
-  contact_number,
-  address = {
+  // age,
+  // contact_number,
+  // address = {
     
-  }
+  // }
 ) {
   const reuired_adv_amount = { for_admission: 1000, for_registration: 500 };
   let admission_amount = reuired_adv_amount.for_admission;
   let registration_amount = reuired_adv_amount.for_registration;
-}
+
 
 if (query) {
-  console.log(`"For admission You have to pay" ${admission_amount}` + "Rs.");
+  console.log(`For admission You have to pay ${admission_amount} Rs.`);
 
   console.log(
     `"For Registration You have to Pay Minimum" ${registration_amount}` + "Rs."
@@ -26,22 +26,22 @@ if (query) {
     console.log("Registration is possible");
   } else {
     console.log(
-      `"Pay atleat" ${
+      `Pay atleat ${
         registration_amount - stu_advance
-      } "to get done registration"`
+      } to get done registration`
     );
     console.info(
       `pay atlest ${admission_amount - stu_advance} to complete admission!!`
     );
   }
-}
+}}
 
 let stu_name = "vishnu",
   stu_advance = 433,
   course_name = "javascript",
   query = false;
-  age = 18;
-  contact_number=8765456987;
-  address={}
+  // age = 18;
+  // contact_number=8765456987;
+  // address={}
 
 admission(stu_name, stu_advance, course_name, query);
