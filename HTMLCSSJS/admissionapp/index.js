4c05eb2689043c55d3571cf0f9a2192295d8fae5
kundan kumar <kundankumarkondil1999@gmail.com>
2024-02-11 16:06:26 +0530

    admission system

---
+++ HTMLCSSJS/admissionapp/index.js
@@ -0,0 +1,47 @@
+function admission(
+  stu_name,
+  stu_advance,
+  course_name,
+  query,
+  age,
+  contact_number,
+  address = {
+    
+  }
+) {
+  const reuired_adv_amount = { for_admission: 1000, for_registration: 500 };
+  let admission_amount = reuired_adv_amount.for_admission;
+  let registration_amount = reuired_adv_amount.for_registration;
+}
+
+if (query) {
+  console.log(`"For admission You have to pay" ${admission_amount}` + "Rs.");
+
+  console.log(
+    `"For Registration You have to Pay Minimum" ${registration_amount}` + "Rs."
+  );
+} else {
+  if (admission_amount <= stu_advance) {
+    console.log("admission is possible");
+    console.log("Registration is possible");
+  } else {
+    console.log(
+      `"Pay atleat" ${
+        registration_amount - stu_advance
+      } "to get done registration"`
+    );
+    console.info(
+      `pay atlest ${admission_amount - stu_advance} to complete admission!!`
+    );
+  }
+}
+
+let stu_name = "vishnu",
+  stu_advance = 433,
+  course_name = "javascript",
+  query = false;
+  age = 18;
+  contact_number=8765456987;
+  address={}
+
+admission(stu_name, stu_advance, course_name, query);
